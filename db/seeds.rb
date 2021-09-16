86e13cf3f0b89778690517e0210563a7bf48395b
sydney <sydneychase5841@gmail.com>
2021-09-15 21:00:20 -0400

    reseeded data with foreign keys in tact

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,34 @@ Book.destroy_all
 BookshelfBook.destroy_all
 
 
-user = User.create!(
+    user1 = User.create!(
         {first_name: 'Sydney', last_name: 'Chase', user_name: 'Indi', email: 'theindigoroom2@gmail.com'}
         )
-# User.create!(
-#         {first_name: 'Jennifer', last_name: 'Johnson', user_name: 'JenJohn', email: 'jenjon@emailexample.com'}
-#         )
-# User.create!(
-#         {first_name: 'Bob', last_name: 'Daily', user_name: 'Beeto', email: 'beeto@emailexample.com'}
-#         )
+    user2 = User.create!(
+        {first_name: 'Jennifer', last_name: 'Johnson', user_name: 'JenJohn', email: 'jenjon@emailexample.com'}
+        )
+    user3 = User.create!(
+        {first_name: 'Bob', last_name: 'Daily', user_name: 'Beeto', email: 'beeto@emailexample.com'}
+        )
+    user4 = User.create!(
+        {first_name: 'Yvonne', last_name: 'Gist', user_name: 'Vonnie', email: 'vonie@emailexample.com'}
+        )
 
 
-the_indigo_room_bookshelf = Bookshelf.create!(
-    {name: 'The Indigo Room Bookshelf', description: 'Here you can find a list of books we have either read or reccommended on the show.', user_id: user.id }
+    bookshelf1 = Bookshelf.create!(
+    {name: 'The Indigo Room Bookshelf', description: 'Here you can find a list of books we have either read or reccommended on the show.', user_id: user1.id }
     )
+    bookshelf2 = Bookshelf.create!(
+    {name: "Jennifer's Beautiful Books", description: 'I love my books. I love sharing great books that I have read with others. Listed are some of my favorites. Will be adding more.', user_id: user2.id }
+    )
+    bookshelf3 = Bookshelf.create!(
+    {name: 'Random', description: 'Random books I like.', user_id: user3.id}
+    )
+    bookshelf4 = Bookshelf.create!(
+    {name: 'DIY Books', description: 'I like to create interior designs and these are the books I love for inspiration.', user_id: user4.id}
+    )
+
+
 mind_garden_of_eden = Book.create!(
     {img_url: 'https://sydneychase.files.wordpress.com/2012/03/cover-mind-garden-of-eden.jpg',
 title: 'Mind Garden of Eden', author: 'Sydney Chase', year_published: 2011, 
@@ -34,39 +48,39 @@ description: 'Re-Conditioning Your Mind to start to accept, express and Imagine 
 read: [true, false]}
 )
 
-the_indigo_room_bookshelf.books << [mind_garden_of_eden]
+spiritual_growth = Book.create!(
+    {img_url: 'https://sydneychase.files.wordpress.com/2021/09/books__spiritual_growth.jpg',
+title: 'Spiritual Growth', author: 'Sanya Roman', year_published: 1987, 
+description: 'Spiritual Growth teaches you how to be your Higher Self',
+read: [true, false]}
+)
 
+resurrection = Book.create!(
+    {img_url: 'https://sydneychase.files.wordpress.com/2021/09/9780875168258.jpg',
+title: 'Resurrection', author: 'Neville Goddard', year_published: 1966, 
+description: 'Feeling is the Secret. Chankging your thoughts and your imagination can indeed change your reality!',
+read: [true, false]}
+)
 
-# books = Book.create!(
-#     {img_url: 'https://sydneychase.files.wordpress.com/2021/09/books__spiritual_growth.jpg',
-# title: 'Spiritual Growth', author: 'Sanya Roman', year_published: '1987', 
-# description: 'Spiritual Growth teaches you how to be your Higher Self',
-# read: [true, false]}
-# )
+the_celestine_prophecy = Book.create!(
+    {img_url: 'https://sydneychase.files.wordpress.com/2021/09/thecelestineprophecy.jpg',
+title: 'The Celestine Prophecy', author: 'James Redfield', year_published: 1993, 
+description: '.The Celestine Prophecy contains secrets that are currently changing the world.',
+read: [true, false]}
+)
 
-# Book.create!(
-#     {img_url: 'https://sydneychase.files.wordpress.com/2021/09/9780875168258.jpg',
-# title: 'Resurrection', author: 'Neville Goddard', year_published: '1966', 
-# description: 'Feeling is the Secret. Chankging your thoughts and your imagination can indeed change your reality!',
-# read: [true, false]}
-# )
+the_kinfolk_home = Book.create!(
+    {img_url: 'https://sydneychase.files.wordpress.com/2021/09/9781579656652_3d.png',
+title: 'The Kinfolk Home Interiors for Slow Living', author: 'Nathan Williams', year_published: 2015, 
+description: "Slow living: cultivating community, simplifying our lives and reclaiming time for what matters most.",
+read: [true, false]}
+)
 
-# Book.create!(
-#     {img_url: 'https://sydneychase.files.wordpress.com/2021/09/thecelestineprophecy.jpg',
-# title: 'The Celestine Prophecy', author: 'James Redfield', year_published: '1993', 
-# description: '.The Celestine Prophecy contains secrets that are currently changing the world.',
-# read: [true, false]}
-# )
-
-
-# Bookshelf.create!(
-#     {name: 'Sydney Chase Bookshelf', description: 'I love my books. I love sharing great books that I have read with others. Listed are some of my favorites. Will be adding more.', user_id: 'user_id' })
-# Bookshelf.create!(
-#     {name: 'Random', description: 'Random books I like.', user_id: 'user_id'})
-
-    
-    
-    
+bookshelf1.books << [mind_garden_of_eden, resurrection]
+bookshelf2.books << [spiritual_growth]
+bookshelf3.books << [resurrection]
+bookshelf4.books << [the_kinfolk_home]
+ 
 
 puts "Books, Bookshelf & Et all seeded"
 
